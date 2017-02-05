3416c87b1e5cfe5810026e93e6844f815c33b4f9
CodingBash <bbecerr@outlook.com>
2017-02-05 14:31:59 -0600

    Added extra line for menu

--- assignment-1/Input.cpp
+++ assignment-1/Input.cpp
@@ -5,6 +5,7 @@ std::vector<int> Input::retrieveInput()
 {
 	std::vector<int> userInput;
 	std::cout << "Enter list of numbers. Type X when complete" << std::endl;
+	std::cout << "One number per line. Press enter after each number entry." << std::endl;
 	bool inputFlag = true;
 	while(inputFlag){
 		std::cout << "Enter number: ";
